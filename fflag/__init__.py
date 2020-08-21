5088cefb839e565ab1f83178bdc375c5cb2672eb
Vladislav Bakin <vladislav@bakin.me>
2020-08-21 18:59:06 +0300

    add app config with verbose name and label

--- fflag/__init__.py
+++ fflag/__init__.py
@@ -1,1 +1,3 @@
 from .models import fflag_enabled  # noqa: F401
+
+default_app_config = 'fflag.apps.FFlagAppConfig'
---
+++ fflag/apps.py
@@ -0,0 +1,8 @@
+from django.apps import AppConfig
+
+
+class FFlagAppConfig(AppConfig):
+    name = 'fflag'
+    label = 'fflag'
+
+    verbose_name = 'FFlag'
